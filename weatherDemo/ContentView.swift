689f2167f52129b0676ecee2effa47b8a88ffb60
xuyuanbei <yuanbei_xu@ssic.cn>
2022-07-19 23:57:19 +0800

    Initial Commit

---
+++ weatherDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherDemo
+//
+//  Created by 许远备 on 2022/7/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherDemo/weatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherDemoApp.swift
+//  weatherDemo
+//
+//  Created by 许远备 on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct weatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
